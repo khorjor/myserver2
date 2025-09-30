bd20dab725fd4495ecd645e0b2dc9e351827ca75
kjbrat <87053833+khorjor@users.noreply.github.com>
2025-09-30 16:20:32 +0700

    dev resolve

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,10 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Utils = require('./Utils');
-if (Utils.add(2, 3) === 5) {
-    console.log(0);
-}
-else {
-    console.log(1);
-}
+//incase we have multiple test
+const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
+    //test1
+    if (Utils.add(2, 3) === 5) {
+        console.log(0);
+    }
+    else {
+        console.log(1);
+    }
+    //test2
+    if (Utils.add(2, 4) === 6) {
+        console.log(0);
+    }
+    else {
+        console.log(1);
+    }
+});
+unit_test();
 //# sourceMappingURL=Test1.js.map
